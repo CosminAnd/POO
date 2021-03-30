4aafc03d9274eabc9ba7d4d399f9c16fe7408869
CosminAnd <79159337+CosminAnd@users.noreply.github.com>
2021-03-30 20:07:49 +0300

    Lab7P2

--- Lab7/P2/main.cpp
+++ Lab7/P2/main.cpp
@@ -1,44 +1,23 @@
 #include <iostream>
 using namespace std;
-
+#define MAX 10001
 template<class  T>
- class vector
- {
-     T* arry;
+class vector
+{
+    T arry[MAX];
      int n;
  public:
-     vector(int x) 
-     { 
-         arry = new T[n]; 
-         n = x; 
-         int a;
-         for (int i = 0; i < n; i++)
-         {
-             cin >> a;
-             arry[i] = a;
-         }
-     };
-
-    /* ~vector()  ????
-     {
-         delete arry; 
-         arry = nullptr; 
-     };*/
-
-     bool operator<(T elem1) ///????
-     {
-         return arry<elem1;
-     };
-
-     void push(T x) 
+     vector() { n = 0;}
+   
+     void push(const T& x) 
      {
          arry[n++] = x; 
      };
 
-     T pop() 
+     T& pop()
      {
-         return arry[--n]; 
-     };
+         return arry[--n];
+     }
 
      void remove(int index)
      {
@@ -47,9 +26,9 @@ template<class  T>
          for (int i=index;i<=n-1;i++)
              arry[i] = arry[i+1];
          n--;
-     };
+     }
 
-     void insert(T elem, int index) 
+     void insert(const T& elem, int index) 
      {
          if (index<0 || index>n - 1)
              return;
@@ -58,62 +37,94 @@ template<class  T>
          arry[index] = elem;
          n++;
      
-     };
+     }
 
-     void sort()  //????
+     void sort(bool (*isLower) (const T& elem1, const T& elem2))  
      {
          bool ok = 0;
          while (!ok)
          {
              ok = 1;
              for (int i = 0; i < n - 1; i++)
-                 if (arry[i] < arry[i + 1])
+             {
+                 if (isLower != nullptr)
                  {
-                     T aux = arry[i];
-                     arry[i] = arry[i + 1];
-                     arry[i + 1] = aux;
-                     ok = 0;
+                     if (isLower(arry[i], arry[i + 1]))
+                     {
+                         T aux = arry[i];
+                         arry[i] = arry[i + 1];
+                         arry[i + 1] = aux;
+                         ok = 0;
+                     }
                  }
+                 else
+                 {
+                     if (arry[i] < arry[i + 1])
+                     {
+                         T aux = arry[i];
+                         arry[i] = arry[i + 1];
+                         arry[i + 1] = aux;
+                         ok = 0;
+                     }
+                 }
+             }
          }
      
-     }; 
+     }
 
-     const T get(int index) /// ?????
+      T& get(int index) 
      {
          if (index<0 || index>n - 1)
              return;
-         return &arry[index];
-     }; 
+         return arry[index];
+     };
 
-     void set(int index,T elem)
+     void set(int index,const T& elem)
      {
          if (index<0 || index>n - 1)
              return;
          arry[index] = elem;
-     };
+     }
 
      int count() 
      {
          return n;
-     };
+     }
 
-     /*void find();/// to do ?????
+     int firstIndexOf(const T& elem, bool (isEqual(const T& e1, const T& e2)))
+     {
+         for(int i=0;i<n;i++)
+         if (isEqual == nullptr)
+         {
+             if (elem == arry[i]) return i;
+         }
+         else
+         {
+             if (isEqual(elem, arry[i])) return i;
+         }
 
-     T firstIndexOf(T elem, void find()) {}; ///to do
-     */
+     }
+    
      void aifs()
      {
          for (int i = 0; i <= n - 1; i++)
              cout << arry[i]<<" ";
-     };
+     }
 };
 
 
 
 int main()
 {
-    vector <int> s(10);
+    vector <int> s;
+    s.push(9);
+    s.push(10);
+    s.push(8);
     s.insert(11, 2);
+    s.pop();
+    cout << s.count()<<"\n";
+    s.sort(nullptr);
+    cout << s.firstIndexOf(11, nullptr)<<"\n";
     s.aifs();
    
     return 0;
